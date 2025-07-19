2679a600d3ab39eaf9127947c18a01c67018805a
Craig D'Silva <craig.dsilva10@gmail.com>
2025-07-19 12:34:09 +0100

    Add dropdown

--- script.mjs
+++ script.mjs
@@ -6,6 +6,4 @@
 
 import { countUsers } from "./common.mjs";
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
-};
+window.onload = function () {};
