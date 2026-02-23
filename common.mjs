344ce36cb20ef0d0b2058ce07133664f3c802439
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-23 11:50:10 +0000

    Most listened song function

--- common.mjs
+++ common.mjs
@@ -1,3 +1,20 @@
-import { getUserIDs } from "./data.mjs";
+import { getListenEvents, getSong, getUserIDs } from "./data.mjs";
 
-export const countUsers = () => getUserIDs().length;
+export const mostListenedtoSong = (userID) => {
+  const countObj = {};
+  getListenEvents(userID).forEach((song) => {
+    if (song.song_id in countObj) {
+      countObj[song.song_id] += 1;
+    } else {
+      countObj[song.song_id] = 1;
+    }
+  });
+  const countArr = [];
+  for (const song in countObj) {
+    countArr.push([song, countObj[song]]);
+  }
+  const sortedCount = countArr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  return getSong(countArr[0][0]);
+};
