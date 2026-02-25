9139a1885e8cdc93d9f0f95315049c6d0bc4226e
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-25 14:57:12 +0000

    mostListenedToSong

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,20 @@
 import assert from "node:assert";
-import test from "node:test";
-import { countUsers } from "./common.mjs";
+import { describe, it } from "node:test";
+import { mostListenedToSong } from "./common.mjs";
 
-test("User count is correct", () => {
-  assert.equal(countUsers(), 4);
+// Test for the most listened to song function
+describe("mostListenedToSong", () => {
+  it("should return the correct most listened to song for user 1", () => {
+    const data = mostListenedToSong(1);
+    assert.equal(
+      data.songByCount,
+      "The Swell Season - When Your Mind's Made Up",
+    );
+    assert.equal(data.songByTime, "Faithless - Insomnia");
+  });
+
+  it("should return only one genre for user 2", () => {
+    const data = mostListenedToSong(2);
+    assert.equal(data.genres.length, 1);
+  });
 });
