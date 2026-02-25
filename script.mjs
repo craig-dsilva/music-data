7355b86344e7cdab88552601badc62920123ac56
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-25 10:52:21 +0000

    Render user ID

--- script.mjs
+++ script.mjs
@@ -1,7 +1,16 @@
-import { getUserIDs } from "./data.mjs";
+import {
+  everyDaySong,
+  fridayNightSong,
+  longestStreak,
+  mostListenedToArtist,
+  mostListenedToSong,
+} from "./common.mjs";
+import { getListenEvents, getUserIDs } from "./data.mjs";
+
+const userDropdown = document.querySelector("#user");
+const userDataEl = document.querySelector("#user-data");
 
 const populateUserDropdown = () => {
-  const userDropdown = document.querySelector("#user");
   getUserIDs().forEach((user) => {
     const userOptionEl = document.createElement("option");
     userOptionEl.value = user;
@@ -10,6 +19,25 @@ const populateUserDropdown = () => {
   });
 };
 
+const renderData = () => {
+  const userID = userDropdown.value;
+  userDataEl.innerHTML = "";
+  const userIDEl = document.createElement("h2");
+  userIDEl.innerText = `User ${userID}`;
+  userDataEl.appendChild(userIDEl);
+
+  // If user has no data
+  if (getListenEvents(userID).length === 0) {
+    const noDataEl = document.createElement("p");
+    noDataEl.innerText = `User ${userID} didn't listen to any songs`;
+    userDataEl.appendChild(noDataEl);
+    return;
+  }
+};
+
+userDropdown.addEventListener("change", () => renderData());
+
 window.onload = () => {
   populateUserDropdown();
+  renderData();
 };
